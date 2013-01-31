e59e405b3435e3c4b9fc4a4264a9bbb581f2107d
Andrew Pope <drewii2ii@gmail.com>
2013-01-31 16:03:39 -0500

    Edited directory.rb to work with a specified JSON file

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,28 +1,14 @@
 class Directory
   
-  def self.crawl
-    require 'open-uri'
+  def self.crawl(path)
+    require 'json'
 
-
-#=begin
-    subjects = Subject.order( "id" )
-    num_subjects = subjects.size
-
-    subjects.each_with_index do |s, index|
-      unless s.abbreviation.nil?
-        url = "http://www.columbia.edu/cu/bulletin/uwb/subj/" + s.abbreviation
-        puts "(" << (index+1).to_s << " of " << num_subjects.to_s << "): " << url
-                
-        begin
-          doc = Nokogiri::HTML(open(url))
-        rescue
-          puts "\t\tBad subject URL"
-          next
-        end
-
-        doc.css('a').each { |a| Section.update_or_create( url + "/" + a.content ) if a.content =~ /[A-Z0-9]+-[0-9]+-[0-9]+/ }
+    json = JSON.parse(File.read(path));
+    json.each_with_index do |section, index|
+      unless section["Term"].nil?
+        puts "(" << (index+1).to_s << " of " << json.length << "): " << section["Course"];
+        Section.update_or_create(section);
       end
     end
-#=end
   end
 end
