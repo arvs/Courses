6160fb6dfd54185f5b2bc784df11dbde6a912002
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2013-04-09 10:32:03 -0400

    made gemfile explicit for rails v3, dropped sphinx to isolate test instance

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-gem 'rails'
-gem 'mysql'
+gem 'rails', '3.2.13'
+gem 'mysql2'
 gem 'json'
-gem 'thinking-sphinx', '2.0.10', :require => 'thinking_sphinx'
+# gem 'thinking-sphinx', '2.0.10', :require => 'thinking_sphinx'
 gem 'thin'
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -22,7 +23,6 @@ ActiveRecord::Schema.define(:version => 20101223050443) do
 
   create_table "departments", :force => true do |t|
     t.string "title"
-    t.string "abbreviation"
   end
 
   create_table "instructors", :force => true do |t|
